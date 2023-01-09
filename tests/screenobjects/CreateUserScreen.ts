4842f2f96a0684c38384ebd55c6364adc28e68cc
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-01-09 17:07:06 -0600

    test(appium): add tests for create user screen

--- tests/screenobjects/CreateUserScreen.ts
+++ tests/screenobjects/CreateUserScreen.ts
@@ -7,8 +7,7 @@ const SELECTORS = {
   PRE_RELEASE_INDICATOR: "~pre-release",
   PRE_RELEASE_INDICATOR_TEXT: "-ios class chain:**/XCUIElementTypeStaticText[`value == \"Pre-release\"`]",
   UNLOCK_LAYOUT: "~unlock-layout",
-  UNLOCK_WARNING_PARAGRAPH: "~unlock-warning-paragraph",
-  UNLOCK_WARNING_SPAN: "~unlock-warning-span",
+  USERNAME_INPUT: "~username-input",
   WINDOW: "-ios class chain:**/XCUIElementTypeWebView",
 }
 
@@ -41,12 +40,8 @@ class CreatePinScreen extends AppScreen {
     return $(SELECTORS.UNLOCK_LAYOUT)
   }
 
-  get unlockWarningParagraph() {
-    return $(SELECTORS.UNLOCK_WARNING_PARAGRAPH).$("-ios class chain:**/XCUIElementTypeStaticText")
-  }
-
-  get unlockWarningSpan() {
-    return $(SELECTORS.UNLOCK_WARNING_SPAN).$("-ios class chain:**/XCUIElementTypeStaticText")
+  get usernameInput() {
+    return $(SELECTORS.USERNAME_INPUT)
   }
 
   get window() {
@@ -55,6 +50,10 @@ class CreatePinScreen extends AppScreen {
   
   async enterPin(pin: string) {
     await this.pinInput.setValue(pin)
+  }
+
+  async enterUsername(username: string) {
+    await this.usernameInput.setValue(username)
   }
 
   async clickOnCreateAccount() {
