9ee5a10d62a229a608cb29dba3ac679f8af10d5f
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-01-30 17:52:50 -0600

    test(appium): add tests and screenobject for settings general

--- tests/screenobjects/SettingsGeneralScreen.ts
+++ tests/screenobjects/SettingsGeneralScreen.ts
@@ -1,7 +1,14 @@
-import SettingsBaseScreen from './SettingsBaseScreen';
+import SettingsBaseScreen from "./SettingsBaseScreen";
 
 const SELECTORS = {
+  CLEAR_THEME_BUTTON: "~clear-theme-button",
+  DROPDOWN_MENU: "~Selector",
+  DROPDOWN_OPTION: "~Selector Option",
+  SETTINGS_CONTROL: "~settings-control",
   SETTINGS_GENERAL: "~settings-general",
+  SETTINGS_INFO: "~settings-info",
+  SETTINGS_SECTION: "~settings-section",
+  SWITCH_SLIDER: "~Switch Slider",
 };
 
 class SettingsGeneralScreen extends SettingsBaseScreen {
@@ -9,8 +16,156 @@ class SettingsGeneralScreen extends SettingsBaseScreen {
     super(SELECTORS.SETTINGS_GENERAL);
   }
 
+  get appLanguageDescription() {
+    return $$(SELECTORS.SETTINGS_SECTION)[4].$(
+      '//*[@label="settings-info"]/*[2]/*[1]'
+    );
+  }
+
+  get appLanguageHeader() {
+    return $$(SELECTORS.SETTINGS_SECTION)[4].$(
+      '//*[@label="settings-info"]/*[1]'
+    );
+  }
+
+  get appLanguageDropdown() {
+    return $$(SELECTORS.SETTINGS_SECTION)[4].$(SELECTORS.DROPDOWN_MENU);
+  }
+
+  get clearThemeButton() {
+    return $(SELECTORS.CLEAR_THEME_BUTTON);
+  }
+
+  get dropdownMenu() {
+    return $(SELECTORS.DROPDOWN_MENU);
+  }
+
+  get dropdownOption() {
+    return $(SELECTORS.DROPDOWN_OPTION);
+  }
+
+  get settingsControl() {
+    return $(SELECTORS.SETTINGS_CONTROL);
+  }
+
   get settingsGeneral() {
     return $(SELECTORS.SETTINGS_GENERAL);
+  }
+
+  get settingsInfo() {
+    return $(SELECTORS.SETTINGS_INFO);
+  }
+
+  get settingsSection() {
+    return $(SELECTORS.SETTINGS_SECTION);
+  }
+
+  get resetThemeDescription() {
+    return $$(SELECTORS.SETTINGS_SECTION)[3].$(
+      '//*[@label="settings-info"]/*[2]/*[1]'
+    );
+  }
+
+  get resetThemeHeader() {
+    return $$(SELECTORS.SETTINGS_SECTION)[3].$(
+      '//*[@label="settings-info"]/*[1]'
+    );
+  }
+
+  get splashScreenControllerButton() {
+    return $$(SELECTORS.SETTINGS_CONTROL)[1].$(SELECTORS.SWITCH_SLIDER);
+  }
+
+  get splashScreenControllerValue() {
+    return $$(SELECTORS.SETTINGS_CONTROL)[1].$(
+      '//*[@label="Switch Slider"]/*[1]'
+    );
+  }
+
+  get splashScreenDescription() {
+    return $$(SELECTORS.SETTINGS_SECTION)[1].$(
+      '//*[@label="settings-info"]/*[2]/*[1]'
+    );
+  }
+
+  get splashScreenHeader() {
+    return $$(SELECTORS.SETTINGS_SECTION)[1].$(
+      '//*[@label="settings-info"]/*[1]'
+    );
+  }
+
+  get themeDescription() {
+    return $$(SELECTORS.SETTINGS_SECTION)[2].$(
+      '//*[@label="settings-info"]/*[2]/*[1]'
+    );
+  }
+
+  get themeDropdown() {
+    return $$(SELECTORS.SETTINGS_SECTION)[2].$(SELECTORS.DROPDOWN_MENU);
+  }
+
+  get themeHeader() {
+    return $$(SELECTORS.SETTINGS_SECTION)[2].$(
+      '//*[@label="settings-info"]/*[1]'
+    );
+  }
+
+  get uplinkOverlayControllerButton() {
+    return $$(SELECTORS.SETTINGS_CONTROL)[0].$(SELECTORS.SWITCH_SLIDER);
+  }
+
+  get uplinkOverlayControllerValue() {
+    return $$(SELECTORS.SETTINGS_CONTROL)[0].$(
+      '//*[@label="Switch Slider"]/*[1]'
+    );
+  }
+
+  get uplinkOverlayDescription() {
+    return $$(SELECTORS.SETTINGS_SECTION)[0].$(
+      '//*[@label="settings-info"]/*[2]/*[1]'
+    );
+  }
+
+  get uplinkOverlayHeader() {
+    return $$(SELECTORS.SETTINGS_SECTION)[0].$(
+      '//*[@label="settings-info"]/*[1]'
+    );
+  }
+
+  get switchSlider() {
+    return $(SELECTORS.SWITCH_SLIDER);
+  }
+
+  async clickOnAppLanguageDropdown() {
+    await this.appLanguageDropdown.click();
+  }
+
+  async clickOnResetTheme() {
+    await this.clearThemeButton.click();
+  }
+
+  async clickOnSplashScreen() {
+    await this.splashScreenControllerButton.click();
+  }
+
+  async clickOnThemeDropdown() {
+    await this.themeDropdown.click();
+  }
+
+  async clickOnUplinkOverlay() {
+    await this.uplinkOverlayControllerButton.click();
+  }
+
+  async selectAppLanguage(language: string) {
+    await $$("-ios class chain:**/XCUIElementTypePopUpButton")[1].addValue(
+      language + "\n"
+    );
+  }
+
+  async selectTheme(theme: string) {
+    await $$("-ios class chain:**/XCUIElementTypePopUpButton")[0].addValue(
+      theme + "\n"
+    );
   }
 }
 
