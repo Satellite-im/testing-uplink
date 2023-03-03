a8b344639f778ebeb1c8631b6516ba346e8ce831
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-03-03 13:47:09 -0600

    test(update): fix block test from context menu

--- tests/specs/04-friends.spec.ts
+++ tests/specs/04-friends.spec.ts
@@ -246,7 +246,7 @@ export default async function friends() {
     await FriendsScreen.openFriendContextMenu(friendName);
 
     // Select last option "Favorites" from Context Menu
-    await FriendsScreen.contextMenuOption[4].click();
+    await FriendsScreen.contextMenuOption[3].click();
 
     // Get current list of All friends and ensure that it does not include the blocked user
     const allFriendsList = await FriendsScreen.getAllFriendsList();
