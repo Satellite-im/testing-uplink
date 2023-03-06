0513cc9febb97daa9aedfab1dd22cfbba8874440
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-03-06 12:32:36 -0600

    chore(appium): updates to appium runner

--- config/wdio.shared.mac.appium.conf.ts
+++ config/wdio.shared.mac.appium.conf.ts
@@ -17,8 +17,6 @@ config.services = (config.services ? config.services : []).concat([
         relaxedSecurity: true,
         // Write the Appium logs to a file in the root of the directory
         log: "./appium.log",
-        address: 'localhost',
-        port: 4723,
       },
     },
   ],
@@ -29,6 +27,6 @@ config.services = (config.services ? config.services : []).concat([
 // =====================
 //
 
-config.port = 10100;
+config.port = 4723;
 
 export default config;
--- config/wdio.windows.app.conf.ts
+++ config/wdio.windows.app.conf.ts
@@ -25,7 +25,6 @@ config.capabilities = [
     // This is `appium:` for all Appium Capabilities which can be found here
     "appium:automationName": "windows",
     "appium:app": join(process.cwd(), "\\apps\\ui.exe"),
-    "appium:systemPort": 4724,
   },
 ];
 
