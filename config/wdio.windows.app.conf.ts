f2d282eecc753fde352a43e484bfee12eb9e5c53
Luis Cardeña Maldonado <lcard54@gmail.com>
2023-03-10 16:27:51 -0600

    chore(appium): update wait for app launch to 30 seconds

--- config/wdio.windows.app.conf.ts
+++ config/wdio.windows.app.conf.ts
@@ -25,6 +25,7 @@ config.capabilities = [
     // This is `appium:` for all Appium Capabilities which can be found here
     "appium:automationName": "windows",
     "appium:app": join(process.cwd(), "\\apps\\ui.exe"),
+    "ms:waitForAppLaunch": 30,
   },
 ];
 
