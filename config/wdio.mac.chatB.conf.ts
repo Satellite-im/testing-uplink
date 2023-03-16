0d612a8419997b3409a3f5b63fcb71999ca4e8cc
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-03-16 15:03:50 -0600

    chore(appium): change setup of appium chat config files

--- config/wdio.mac.chatB.conf.ts
+++ config/wdio.mac.chatB.conf.ts
@@ -1,8 +1,37 @@
 const fsp = require("fs").promises;
 const mkdirp = require("mkdirp");
 
-import config from "./wdio.shared.mac.appium.conf";
+import { config } from "./wdio.shared.conf";
 import { join } from "path";
+
+//
+// ======
+// Appium
+// ======
+//
+config.services = (config.services ? config.services : []).concat([
+  [
+    "appium",
+    {
+      // This will use the globally installed version of Appium
+      command: "appium",
+      args: {
+        // This is needed to tell Appium that we can execute local ADB commands
+        // and to automatically download the latest version of ChromeDriver
+        relaxedSecurity: true,
+        // Write the Appium logs to a file in the root of the directory
+        log: "./appium.log",
+      },
+    },
+  ],
+]);
+//
+// =====================
+// Server Configurations
+// =====================
+//
+
+config.port = 10102;
 
 // ============
 // Specs
--- config/wdio.windows.chatA.conf.ts
+++ config/wdio.windows.chatA.conf.ts
@@ -1,9 +1,36 @@
-import config from "./wdio.shared.windows.appium.conf";
+import { config } from "./wdio.shared.conf";
 import { join } from "path";
-
 
 const fsp = require("fs").promises;
 const mkdirp = require("mkdirp");
+
+//
+// ======
+// Appium
+// ======
+//
+config.services = (config.services ? config.services : []).concat([
+  [
+    "appium",
+    {
+      // This will use the globally installed version of Appium
+      command: "appium",
+      args: {
+        // This is needed to tell Appium that we can execute local ADB commands
+        // and to automatically download the latest version of ChromeDriver
+        relaxedSecurity: true,
+        // Write the Appium logs to a file in the root of the directory
+        log: "./appium.log",
+      },
+    },
+  ],
+]);
+//
+// =====================
+// Server Configurations
+// =====================
+//
+config.port = 4725;
 
 // ============
 // Specs
