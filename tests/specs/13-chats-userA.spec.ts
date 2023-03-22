ae541f19c7c878a1addc29fdf2e6a57564ee77a0
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-03-22 15:30:31 -0600

    test(update): new test accounts for chat

--- tests/specs/13-chats-userA.spec.ts
+++ tests/specs/13-chats-userA.spec.ts
@@ -1,4 +1,4 @@
-import { resetAndLoginWithCache } from "../helpers/commands";
+import { loginWithTestUser } from "../helpers/commands";
 import ChatScreen from "../screenobjects/ChatScreen";
 import FriendsScreen from "../screenobjects/FriendsScreen";
 import WelcomeScreen from "../screenobjects/WelcomeScreen";
@@ -7,7 +7,7 @@ const userBKey = "did:key:z6MksTuCXHc1uy9DqyPaaWjSXwi2ZUcQkSoVgdLxg8GKPuWW";
 describe("Two users at the same time - Chat User A", async () => {
   it("Load Chat User A account, add Chat User B as friend and then send a message", async () => {
     // Go to Friends Screen
-    await resetAndLoginWithCache("ChatUserA");
+    await loginWithTestUser();
     await WelcomeScreen.goToFriends();
 
     // Go to Friends
