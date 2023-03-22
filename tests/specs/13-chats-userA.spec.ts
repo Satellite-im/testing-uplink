6cef42b47d82d9fbbfee8918f7795c140e2da4c7
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-03-21 23:16:19 -0600

    chore(appium): add appium as dev dep

--- tests/specs/13-chats-userA.spec.ts
+++ tests/specs/13-chats-userA.spec.ts
@@ -13,9 +13,6 @@ describe("Two users at the same time - Chat User A", async () => {
     // Go to Friends
     await FriendsScreen.waitForIsShown(true);
 
-    // Wait for some time until user B is logged in
-    await browser.pause(90000);
-
     // Send friend request to user B
     await FriendsScreen.addSomeoneInput.setValue(userBKey);
     await FriendsScreen.addSomeoneButton.click();
