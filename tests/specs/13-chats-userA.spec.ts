a22d3e17680857098e2484bbeaddd836d1522e0b
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-03-22 11:27:58 -0600

    test(appium): update tests for chat user a and b

--- tests/specs/13-chats-userA.spec.ts
+++ tests/specs/13-chats-userA.spec.ts
@@ -15,7 +15,6 @@ describe("Two users at the same time - Chat User A", async () => {
 
     // Send friend request to user B
     await FriendsScreen.addSomeoneInput.setValue(userBKey);
-    await browser.pause(30000); // wait for 30 seconds before sending friend request;
     await FriendsScreen.addSomeoneButton.click();
 
     // Validate Toast Notification is displayed
@@ -25,9 +24,14 @@ describe("Two users at the same time - Chat User A", async () => {
     ).waitForDisplayed({ reverse: true });
 
     // Wait until friend request is accepted and go to a Chat Conversation with Chat User B
+    await FriendsScreen.goToPendingFriendsList();
     await (
-      await FriendsScreen.chatWithFriendButton
-    ).waitForExist({ timeout: 120000 });
+      await FriendsScreen.removeOrDenyFriendButton
+    ).waitForExist({ reverse: true, timeout: 180000 });
+
+    // Go to All Friends List and validate Chat with Friend Button is displayed
+    await FriendsScreen.goToAllFriendsList();
+    await (await FriendsScreen.chatWithFriendButton).waitForExist();
   });
 
   it("Go to chat with friend and send a message", async () => {
