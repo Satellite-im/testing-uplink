15bf3dec334dfef02e8a50584aad219875c1fff5
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-03-28 14:19:30 -0600

    chore(appium): spec file retries to zero

--- config/wdio.shared.conf.ts
+++ config/wdio.shared.conf.ts
@@ -100,13 +100,13 @@ export const config: WebdriverIO.Config = {
   // before running any tests.
   framework: "mocha",
   // The number of times to retry the entire specfile when it fails as a whole
-  specFileRetries: 1,
+  specFileRetries: 0,
   //
   // Delay in seconds between the spec file retry attempts
-  specFileRetriesDelay: 10,
+  //specFileRetriesDelay: 10,
   //
   // Whether or not retried specfiles should be retried immediately or deferred to the end of the queue
-  specFileRetriesDeferred: false,
+  //specFileRetriesDeferred: false,
   //
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
