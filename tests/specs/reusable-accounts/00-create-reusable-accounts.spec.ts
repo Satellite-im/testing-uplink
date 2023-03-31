ad9d48e0049fb1edf3ce76d73aaad9b350700b32
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-03-31 16:55:11 -0600

    test(update): fix reusable accounts test for chats tests

--- tests/specs/reusable-accounts/00-create-reusable-accounts.spec.ts
+++ tests/specs/reusable-accounts/00-create-reusable-accounts.spec.ts
@@ -3,46 +3,60 @@ import FriendsScreen from "../../screenobjects/FriendsScreen";
 import {
   createNewUser,
   grabCacheFolder,
+  resetAndLoginWithCache,
   resetApp,
 } from "../../helpers/commands";
 import WelcomeScreen from "../../screenobjects/WelcomeScreen";
-let userAKey: string = "";
+let userAKey, userBKey;
 
-// Skipped since offline messaging is not ready yet
-xdescribe("Create Reusable Accounts and Save Data - One instance at a time", () => {
+describe("Create Reusable Accounts and Save Data - One instance at a time", () => {
   it("Create a User A reusable account", async () => {
-    await createNewUser("TestUserA");
+    await createNewUser("ChatUserA");
     await WelcomeScreen.goToFriends();
     await FriendsScreen.clickOnCopyID();
-    userAKey = await execSync("pbpaste", { encoding: "utf8" });
-    await grabCacheFolder("TestUserA");
+    userAKey = await getClipbboardMacOS();
+    await grabCacheFolder("ChatUserA");
     await resetApp();
   });
 
   it("Create a User B reusable account", async () => {
-    await createNewUser("TestUserB");
+    await createNewUser("ChatUserB");
     await WelcomeScreen.goToFriends();
     await FriendsScreen.clickOnCopyID();
+    await FriendsScreen.enterCopiedID();
+    userBKey = await getClipbboardMacOS();
+    console.log("UserBKey is: " + userBKey);
   });
 
   it("User B sends friend request to User A", async () => {
     // Retrieve DID Key for User A using helper function
-    await FriendsScreen.addSomeoneInput.setValue(userAKey);
-    const abbreviatedFriendKey = FriendsScreen.getAbbreviatedDidKey(userAKey);
+    await FriendsScreen.enterFriendDidKey(userAKey);
     await FriendsScreen.addSomeoneButton.click();
 
     // Validate Toast Notification
-    await $("~Toast Notification").waitForDisplayed();
-    const toastContent = await $("~Toast Notification").$("//*[2]/*[1]");
-    const toastClose = await $("~Toast Notification").$("//*[3]");
-    await expect(toastContent).toHaveTextContaining("Friend Request Sent!");
-    await toastClose.click();
-    await $("~Toast Notification").waitForDisplayed({ reverse: true });
+    expect(await FriendsScreen.toastNotificationText).toHaveTextContaining(
+      "Friend Request Sent!"
+    );
+    await (
+      await FriendsScreen.toastNotification
+    ).waitForDisplayed({ reverse: true });
 
     // Go to pending friend requests screen and validate friend request sent
-    await (await FriendsScreen.pendingFriendsButton).click();
+    await FriendsScreen.goToPendingFriendsList();
+    const abbreviatedFriendKey = await FriendsScreen.getAbbreviatedDidKey(
+      userAKey
+    );
     const outgoingList = await FriendsScreen.getOutgoingList();
     expect(outgoingList.includes(abbreviatedFriendKey)).toEqual(true);
-    await grabCacheFolder("TestUserB");
+    await grabCacheFolder("ChatUserB");
+  });
+
+  it("User A accepts friend request to User B", async () => {
+    await resetAndLoginWithCache("ChatUserA");
+    await WelcomeScreen.goToFriends();
+
+    // Go to pending friend requests screen and validate friend request sent
+    await FriendsScreen.goToPendingFriendsList();
+    await browser.pause(10000);
   });
 });
