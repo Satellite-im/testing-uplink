be1de121201313d3f1cecda13d3e191c81e52fbc
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-04-14 11:13:17 -0600

    test(update): add new friends tests account

--- tests/suites/02-UplinkFriends.suite.ts
+++ tests/suites/02-UplinkFriends.suite.ts
@@ -2,5 +2,5 @@ import friends from "../specs/04-friends.spec";
 
 // Note: The following test suite is not running for now on Windows CI due to appium issues that need investigation
 describe("Uplink UI Automated Tests with Reusable Account", function () {
-  xdescribe("Friends Screen Tests", friends.bind(this));
+  describe("Friends Screen Tests", friends.bind(this));
 });
