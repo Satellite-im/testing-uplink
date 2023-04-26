762fe9d094d3521d1b93c73fe37448b528201fc3
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-04-26 14:06:42 -0600

    test(update): add labeler step and retry step on tests

--- config/wdio.shared.conf.ts
+++ config/wdio.shared.conf.ts
@@ -100,7 +100,7 @@ export const config: WebdriverIO.Config = {
   // before running any tests.
   framework: "mocha",
   // The number of times to retry the entire specfile when it fails as a whole
-  specFileRetries: 1,
+  specFileRetries: 0,
   //
   // Delay in seconds between the spec file retry attempts
   specFileRetriesDelay: 60,
