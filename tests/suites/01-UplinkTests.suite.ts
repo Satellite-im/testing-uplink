139882b6b59ee52a45a9f5a35f6b33e7517fd4e0
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-04-26 22:13:15 -0600

    chore(appium): update webdriverio and deps to latest

--- tests/suites/01-UplinkTests.suite.ts
+++ tests/suites/01-UplinkTests.suite.ts
@@ -11,7 +11,6 @@ import settingsNotifications from "../specs/11-settings-notifications.spec";
 import settingsDeveloper from "../specs/12-settings-developer.spec";
 import settingsAccessibility from "../specs/13-settings-accessibility.spec";
 import settingsAbout from "../specs/14-settings-about.spec";
-import { deleteCache } from "../helpers/commands";
 
 describe("Uplink UI Automated Tests", function () {
   describe("Create Pin and Account Tests", createAccount.bind(this));
