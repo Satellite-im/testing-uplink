2a05645e16d8c5468046e64eb4f7074bd2f871da
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-05-10 13:00:56 -0600

    chore(appium): add chats testing using a single instance

--- config/wdio.mac.multiremote.conf.ts
+++ config/wdio.mac.multiremote.conf.ts
@@ -1,183 +1,45 @@
-import { join } from "path";
 const fsp = require("fs").promises;
 
-/**
- * All not needed configurations, for this boilerplate, are removed.
- * If you want to know which configuration options you have then you can
- * check https://webdriver.io/docs/configurationfile
- */
-export const config: WebdriverIO.Config = {
-    //
-    // ====================
-    // Runner Configuration
-    // ====================
-    // WebdriverIO allows it to run your tests in arbitrary locations (e.g. locally or
-    // on a remote machine).
-    runner: "local",
-    //
-    // ==================
-    // Specify Test Files
-    // ==================
-    // The test-files are specified in:
-    // - wdio.android.browser.conf.ts
-    // - wdio.android.app.conf.ts
-    // - wdio.ios.browser.conf.ts
-    // - wdio.ios.app.conf.ts
-    //
-    /**
-     * NOTE: This is just a place holder and will be overwritten by each specific configuration
-     */
-    specs: [join(process.cwd(), "./tests/specs/reusable-accounts/05-chats-with-one-file.ts")],
-    //
-    // ============
-    // Capabilities
-    // ============
-    // The capabilities are specified in:
-    // - wdio.android.browser.conf.ts
-    // - wdio.android.app.conf.ts
-    // - wdio.ios.browser.conf.ts
-    // - wdio.ios.app.conf.ts
-    //
-    /**
-     * NOTE: This is just a place holder and will be overwritten by each specific configuration
-     */
-    maxInstances: 10,
-    capabilities: {
+import config from "./wdio.shared.mac.appium.conf";
+import { join } from "path";
 
-        a: {
-            "host": "localhost",
-            "port": 4724,
-            capabilities: {
-                platformName: "mac",
-                "appium:automationName": "mac2",
-                "appium:bundleId": "com.apple.calculator"
-            }
-        },
-        b: {
-            "host": "localhost",
-            "port": 4723,
-            capabilities: {
-                platformName: "mac",
-                "appium:automationName": "mac2",
-                "appium:bundleId": "com.apple.AppStore",
-            }
-        }
+// ============
+// Specs
+// ============
+config.specs = [join(process.cwd(), "./tests/specs/reusable-accounts/05-chats-with-one-file.spec.ts")];
+
+// ============
+// Capabilities
+// ============
+// For all capabilities please check
+// http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
+config.capabilities = [
+  {
+    // The defaults you need to have in your config
+    platformName: "mac",
+    // For W3C the appium capabilities need to have an extension prefix
+    // http://appium.io/docs/en/writing-running-appium/caps/
+    // This is `appium:` for all Appium Capabilities which can be found here
+    "appium:automationName": "mac2",
+    // @ts-ignore
+    "appium:serverStartupTimeout": 240000,
+    "appium:prerun": {
+      command: 'do shell script "rm -rf ~/.uplink"',
     },
-    //
-    // ===================
-    // Test Configurations
-    // ===================
-    // Define all options that are relevant for the WebdriverIO instance here
-    //
-    // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: "error",
-    // Set specific log levels per logger
-    // loggers:
-    // - webdriver, webdriverio
-    // - @wdio/applitools-service, @wdio/browserstack-service, @wdio/devtools-service, @wdio/sauce-service
-    // - @wdio/mocha-framework, @wdio/jasmine-framework
-    // - @wdio/local-runner
-    // - @wdio/sumologic-reporter
-    // - @wdio/cli, @wdio/config, @wdio/utils
-    // Level of logging verbosity: trace | debug | info | warn | error | silent
-    // logLevels: {
-    //     webdriver: 'info',
-    //     '@wdio/applitools-service': 'info'
-    // },
-    //
-    // If you only want to run your tests until a specific amount of tests have failed use
-    // bail (default is 0 - don't bail, run all tests).
-    bail: 0,
-    // Set a base URL in order to shorten url command calls. If your `url` parameter starts
-    // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
-    // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
-    // gets prepended directly.
-    baseUrl: "http://localhost",
-    // Default timeout for all waitFor* commands.
-    /**
-     * NOTE: This has been increased for more stable Appium Native app
-     * tests because they can take a bit longer.
-     */
-    waitforTimeout: 10000,
-    // Default timeout in milliseconds for request
-    // if browser driver or grid doesn't send response
-    connectionRetryTimeout: 120000,
-    // Default request retries count
-    connectionRetryCount: 3,
-    // Test runner services
-    // Services take over a specific job you don't want to take care of. They enhance
-    // your test setup with almost no effort. Unlike plugins, they don't add new
-    // commands. Instead, they hook themselves up into the test process.
-    //
-    // Services are empty here but will be defined in the
-    // - wdio.shared.browserstack.conf.ts
-    // - wdio.shared.local.appium.conf.ts
-    // - wdio.shared.sauce.conf.ts
-    // configuration files
-    //services: [["appium", { command: "appium", port: [4723, 4724], args: { relaxedSecurity: true, log: "./appiumUserA.log" }}]],
-    // Framework you want to run your specs with.
-    // The following are supported: Mocha, Jasmine, and Cucumber
-    // see also: https://webdriver.io/docs/frameworks
-    //
-    // Make sure you have the wdio adapter package for the specific framework installed
-    // before running any tests.
-    framework: "mocha",
-    // The number of times to retry the entire specfile when it fails as a whole
-    specFileRetries: 0,
-    //
-    // Delay in seconds between the spec file retry attempts
-    //specFileRetriesDelay: 60,
-    //
-    // Whether or not retried specfiles should be retried immediately or deferred to the end of the queue
-    //specFileRetriesDeferred: false,
-    //
-    // Test reporter for stdout.
-    // The only one supported by default is 'dot'
-    // see also: https://webdriver.io/docs/dot-reporter
-    reporters: [[
-      "spec", 
-      {
-        showPreface: false,
-      },
-    ], ['junit', {
-      outputDir: './test-report/',
-      outputFileFormat: function (options) {
-        return `test-results-${options.capabilities.platformName}-${options.cid}.xml`;
-      }
-  }]],
-    // Options to be passed to Mocha.
-    mochaOpts: {
-      ui: "bdd",
-      /**
-       * NOTE: This has been increased for more stable Appium Native app
-       * tests because they can take a bit longer.
-       */
-      timeout: 120000, // 1min
-    },
-  
-    //
-    // =====
-    // Hooks
-    // =====
-    // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
-    // it and to build services around it. You can either apply a single function or an array of
-    // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
-    // resolved to continue.
-    //
-    /**
-     * NOTE: No Hooks are used in this project, but feel free to add them if you need them.
-     */
-    afterTest: async function (test, describe, { error }) {
-        if (error) {
-          let imageFile = await driver.takeScreenshot();
-          let imageFolder = join(process.cwd(), "./test-results/macos", test.parent);
-          await fsp.mkdir(imageFolder, {recursive: true});
-          await fsp.writeFile(
-            imageFolder + "/" + test.title + " - Failed.png",
-            imageFile,
-            "base64"
-          );
-        }
-      },
-  };
-  
+  },
+];
+
+config.afterTest = async function (test, describe, { error }) {
+  if (error) {
+    let imageFile = await driver.takeScreenshot();
+    let imageFolder = join(process.cwd(), "./test-results/macos", test.parent);
+    await fsp.mkdir(imageFolder, {recursive: true});
+    await fsp.writeFile(
+      imageFolder + "/" + test.title + " - Failed.png",
+      imageFile,
+      "base64"
+    );
+  }
+};
+
+exports.config = config;
---
+++ tests/specs/reusable-accounts/05-chats-with-one-file.spec.ts
@@ -0,0 +1,88 @@
+import {
+  createNewUser,
+  getUserKey,
+  saveTestKeys,
+} from "../../helpers/commands";
+import FriendsScreen from "../../screenobjects/friends/FriendsScreen";
+import SettingsProfileScreen from "../../screenobjects/settings/SettingsProfileScreen";
+import WelcomeScreen from "../../screenobjects/welcome-screen/WelcomeScreen";
+
+describe("Two users in one file", () => {
+  it("Chat User A - Create Account", async () => {
+    await driver.executeScript("macos: launchApp", [
+      {
+        bundleId: "im.satellite.uplink05092023",
+      },
+    ]);
+    const username = "ChatUserA";
+    await createNewUser(username);
+    await WelcomeScreen.goToSettings();
+    await SettingsProfileScreen.waitForIsShown(true);
+
+    // Click on Copy ID button and assert Toast Notification is displayed
+    await SettingsProfileScreen.clickOnCopyIDButton();
+
+    // Wait for toast notification to be closed
+    await SettingsProfileScreen.waitUntilNotificationIsClosed();
+
+    // Paste copied DID Key into Status Input
+    await SettingsProfileScreen.pasteUserKeyInStatus();
+    const inputTextElement = await SettingsProfileScreen.getStatusInputText();
+    const didkey = await inputTextElement.getText();
+
+    // Grab cache folder and restart
+    await saveTestKeys(username, didkey);
+    await SettingsProfileScreen.goToFriends();
+    await FriendsScreen.waitForIsShown(true);
+  });
+
+  it("Chat User B - Create Account", async () => {
+    // Go to app store app now
+    await driver.executeScript("macos: launchApp", [
+      {
+        bundleId: "im.satellite.uplink05102023",
+      },
+    ]);
+    const username = "ChatUserB";
+    await createNewUser(username);
+  });
+
+  it("Chat User B - Send friend request to User A", async () => {
+    // Go to Friends
+    await WelcomeScreen.goToFriends();
+    await FriendsScreen.waitForIsShown(true);
+    // Obtain did key from Chat User B
+    const friendDidKey = await getUserKey("ChatUserA");
+    await FriendsScreen.enterFriendDidKey(friendDidKey);
+    await FriendsScreen.clickOnAddSomeoneButton();
+
+    // Wait for toast notification to be closed
+    await FriendsScreen.waitUntilNotificationIsClosed();
+  });
+
+  it("Chat User A - Accept friend request from B", async () => {
+    await driver.executeScript("macos: activateApp", [
+      {
+        bundleId: "im.satellite.uplink05092023",
+      },
+    ]);
+    // Go to pending requests list, wait for receiving the friend request and accept it
+    await FriendsScreen.goToPendingFriendsList();
+    await FriendsScreen.waitUntilFriendRequestIsReceived();
+    await FriendsScreen.acceptIncomingRequest("ChatUserB");
+
+    // Return to Friends List
+    await FriendsScreen.goToAllFriendsList();
+  });
+
+  it("Chat User B - Validate friend request was accepted", async () => {
+    // Return to a app
+    await driver.executeScript("macos: activateApp", [
+      {
+        bundleId: "im.satellite.uplink05102023",
+      },
+    ]);
+    // Go to pending requests list, wait for receiving the friend request and accept it
+    await FriendsScreen.waitUntilUserAcceptedFriendRequest();
+  });
+});
