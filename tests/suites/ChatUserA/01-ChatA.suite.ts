9fc6a5614cd6d06db4e7f0c35adf60702273072e
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-05-15 22:32:40 -0600

    test(update): update suite files[

--- tests/suites/ChatUserA/01-ChatA.suite.ts
+++ tests/suites/ChatUserA/01-ChatA.suite.ts
@@ -1,7 +1,9 @@
-import sendRequest from "../../specs/reusable-accounts/ChatUserA/01-send-request-and-chat.spec";
 import quickProfileUserA from "../../specs/reusable-accounts/ChatUserA/02-quick-profile-with-A.spec";
+import sendRequest from "../../specs/reusable-accounts/ChatUserA/01-send-request-and-chat.spec";
+import sidebarWithUserA from "../../specs/reusable-accounts/ChatUserA/03-sidebar-with-A.spec";
 
 describe("Chat and Friends Tests with User A", function () {
   describe("Send request and chats tests with User A", sendRequest.bind(this));
   describe("Quick Profile Tests with User A", quickProfileUserA.bind(this));
+  describe("Sidebar Tests with User A", sidebarWithUserA.bind(this));
 });
--- tests/suites/ChatUserB/01-ChatB.suite.ts
+++ tests/suites/ChatUserB/01-ChatB.suite.ts
@@ -1,5 +1,6 @@
 import acceptRequest from "../../specs/reusable-accounts/ChatUserB/01-accept-request-and-chat.spec";
 import quickProfileUserB from "../../specs/reusable-accounts/ChatUserB/02-quick-profile-with-B.spec";
+import sidebarWithUserB from "../../specs/reusable-accounts/ChatUserB/03-sidebar-with-B.spec";
 
 describe("Chat and Friends Tests with User B", function () {
   describe(
@@ -7,4 +8,5 @@ describe("Chat and Friends Tests with User B", function () {
     acceptRequest.bind(this)
   );
   describe("Quick Profile Tests with User B", quickProfileUserB.bind(this));
+  describe("Sidebar Tests with User B", sidebarWithUserB.bind(this));
 });
