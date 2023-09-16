8adba325a9a1447f07b33c25f005b0f072f512b9
Luis Cardeña Maldonado <lcard54@gmail.com>
2023-09-15 23:17:22 -0600

    test(update): skipping only group chat tests

--- tests/suites/Chats/01-Chats.suite.ts
+++ tests/suites/Chats/01-Chats.suite.ts
@@ -23,10 +23,10 @@ describe("Windows Chats Tests", function () {
   describe("Chat Tooltips Tests", chatTooltipsTests.bind(this));
   describe("Quick Profile Tests", quickProfileTests.bind(this));
   describe("Sidebar Chats Tests", sidebarChatsTests.bind(this));
-  describe("Group Chats Tests", groupChatTests.bind(this));
+  /*describe("Group Chats Tests", groupChatTests.bind(this));
   describe("Group Chats Edit Tests", groupChatEditTests.bind(this));
   describe(
     "Group Chats Favorites and Sidebar Tests",
     groupChatSidebarTests.bind(this)
-  );
+  );*/
 });
