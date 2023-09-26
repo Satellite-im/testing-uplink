9727a45dc5102ce065faa8f39ae17ce1d0ffb579
Luis Cardeña Maldonado <lcard54@gmail.com>
2023-09-26 11:00:01 -0600

    chore(appium): add one time script to login several times

---
+++ tests/specs/scripts/01-multiple-logins.spec.ts
@@ -0,0 +1,23 @@
+import {
+  createNewUser,
+  closeApplication,
+  launchApplication,
+  loginWithTestUser,
+} from "@helpers/commands";
+import { USER_A_INSTANCE } from "@helpers/constants";
+
+describe("One time script test for login", async () => {
+  it("Should create an account", async () => {
+    const username = "test";
+    await createNewUser(username);
+    await closeApplication(USER_A_INSTANCE);
+  });
+
+  for (let i = 1; i <= 10; i++) {
+    it("Should login to app and reset - Attempt #" + i, async () => {
+      await launchApplication(USER_A_INSTANCE);
+      await loginWithTestUser();
+      await closeApplication(USER_A_INSTANCE);
+    });
+  }
+});
