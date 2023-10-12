95b7fbdb3b596d7d5cc8eefcbe58650dd3451978
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-10-12 08:34:49 -0600

    test(update): skip replies tests for now

--- tests/suites/Chats/01-Chats.suite.ts
+++ tests/suites/Chats/01-Chats.suite.ts
@@ -17,9 +17,9 @@ describe("Windows Chats Tests", function () {
     createChatAccountsTests.bind(this)
   );
   describe("Chat Replies Tests", repliesTests.bind(this));
-  describe("Message Context Menu Tests", messageContextMenuTests.bind(this));
+  /*describe("Message Context Menu Tests", messageContextMenuTests.bind(this));
   describe("Message Input Tests", messageInputTests.bind(this));
-  /*describe("Message Attachments Tests", messageAttachmentsTests.bind(this));
+  describe("Message Attachments Tests", messageAttachmentsTests.bind(this));
   describe("Chat Topbar Tests", chatTopbarTests.bind(this));
   describe("Quick Profile Tests", quickProfileTests.bind(this));
   describe("Sidebar Chats Tests", sidebarChatsTests.bind(this));
