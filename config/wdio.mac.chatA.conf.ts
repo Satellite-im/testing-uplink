1c24a5889df35f1630cd071d5b4304c98b60ad52
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2023-10-30 23:42:35 -0600

    chore(appium): use two GH runners to run chats tests on macos

---
+++ config/wdio.mac.chatA.conf.ts
@@ -0,0 +1,144 @@
+import "module-alias/register";
+import allureReporter from '@wdio/allure-reporter'
+import { config as sharedConfig } from '@config/wdio.shared.conf';
+import { homedir } from "os";
+import { join } from "path";
+import { MACOS_BUNDLE_ID, MACOS_DRIVER } from "@helpers/constants";
+const fsp = require("fs").promises;
+const { readFileSync, rmSync } = require("fs");
+
+// @ts-expect-error
+export const config: WebdriverIO.Config = {
+  ...sharedConfig, 
+  ...{
+    //
+    // ==================
+    // Specify Test Files
+    // ==================
+    // Define which test specs should run. The pattern is relative to the directory
+    // from which `wdio` was called.
+    //
+    // The specs are defined as an array of spec files (optionally using wildcards
+    // that will be expanded). The test for each spec file will be run in a separate
+    // worker process. In order to have a group of spec files run in the same worker
+    // process simply enclose them in an array within the specs array.
+    //
+    // If you are calling `wdio` from an NPM script (see https://docs.npmjs.com/cli/run-script),
+    // then the current working directory is where your `package.json` resides, so `wdio`
+    // will be called from there.
+    //
+    specs: [join(process.cwd(), "./tests/suites/Chats/02-ChatsUserA.suite.ts")],
+    // Patterns to exclude.
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    specFileRetries: 0,
+    //
+    // ============
+    // Capabilities
+    // ============
+    // Define your capabilities here. WebdriverIO can run multiple capabilities at the same
+    // time. Depending on the number of capabilities, WebdriverIO launches several test
+    // sessions. Within your capabilities you can overwrite the spec and exclude options in
+    // order to group specific specs to a specific capability.
+    //
+    //
+    // If you have trouble getting all important capabilities together, check out the
+    // Sauce Labs platform configurator - a great tool to configure your capabilities:
+    // https://docs.saucelabs.com/reference/platforms-configurator
+    //
+    port: 4723,
+    path: '/',
+    capabilities: {
+      userA: {
+        capabilities: {
+          platformName: "mac",
+          "appium:automationName": MACOS_DRIVER,
+          "appium:bundleId": MACOS_BUNDLE_ID,
+          "appium:arguments": ["--path", join(process.cwd() + "/tests/fixtures/users/mac2/ChatUserA")],
+          "appium:systemPort": 4724, 
+        }
+      },
+    }
+    ,
+    //
+    // ===================
+    // Test Configurations
+    // ===================
+    // Define all options that are relevant for the WebdriverIO instance here
+    //
+    reporters: [
+      ["spec", 
+        {
+          showPreface: false,
+        },
+      ], 
+      ['allure', 
+        {
+          outputDir: './allure-results',
+          disableWebdriverStepsReporting: true,
+          disableWebdriverScreenshotsReporting: false,
+        }
+      ],
+      ['junit', 
+        {
+          outputDir: './test-report/',
+          outputFileFormat: function (options) {
+            return `test-results-macos-chatA-${options.cid}.xml`;
+          }
+        }
+      ],
+    ],    
+    //
+    // =====
+    // Hooks
+    // =====
+    // WebdriverIO provides several hooks you can use to interfere with the test process in order to enhance
+    // it and to build services around it. You can either apply a single function or an array of
+    // methods to it. If one of them returns with a promise, WebdriverIO will wait until that promise got
+    // resolved to continue.
+    onPrepare: async function() {
+      const cacheFolder = homedir() + "/.uplink/.user";
+      const allureResultsFolder = join(process.cwd(), "./allure-results");
+      const testReportFolder =  join(process.cwd(), "./test-report");
+      const testResultsFolder =  join(process.cwd(), "./test-results");
+      try {
+        await rmSync(allureResultsFolder, { recursive: true, force: true });
+        await rmSync(testReportFolder, { recursive: true, force: true });
+        await rmSync(testResultsFolder, { recursive: true, force: true });
+        console.log("Deleted Artifacts Folders Successfully!");
+      } catch (error) {
+        console.error(
+            `Got an error trying to delete artifacts folders: ${error.message}`
+        );
+      }
+      try {
+        await rmSync(cacheFolder, { recursive: true, force: true });
+        console.log("Deleted Cache Folder Successfully!");
+      } catch (error) {
+        console.error(
+          `Got an error trying to delete Cache Folder: ${error.message}`
+        );
+      }
+    },
+    
+    afterTest: async function (test, describe, { error }) {
+      if (error) {
+        let imageFile = await driver.takeScreenshot();
+        const imageFolder = join(process.cwd(), "./test-results/macos-chatA", test.parent);
+        const imageTitle = test.title + " - Failed.png"
+        await fsp.mkdir(imageFolder, {recursive: true});
+        await fsp.writeFile(
+          imageFolder + "/" + imageTitle,
+          imageFile,
+          "base64"
+        );
+
+        // Add to Screenshot to Allure Reporter
+        const data = await readFileSync(`${imageFolder}/${imageTitle}`);
+        allureReporter.addAttachment(imageTitle, data, 'image/png')
+      }
+    },
+  }
+    
+}
---
+++ tests/specs/reusable-accounts/ChatUserB/01-create-account.spec.ts
@@ -0,0 +1,20 @@
+import "module-alias/register";
+import {
+  loginWithTestUser,
+} from "@helpers/commands";
+import { USER_A_INSTANCE } from "@helpers/constants";
+import FriendsScreen from "@screenobjects/friends/FriendsScreen";
+import WelcomeScreen from "@screenobjects/welcome-screen/WelcomeScreen";
+let friendsScreenFirstUser = new FriendsScreen(USER_A_INSTANCE);
+let welcomeScreenFirstUser = new WelcomeScreen(USER_A_INSTANCE);
+
+
+export default async function createChatAccountsTests() {
+  it("Chat User B - Login with account previously created", async () => {
+    // Login with account previously created
+    await loginWithTestUser();
+    await welcomeScreenFirstUser.validateWelcomeScreenIsShown();
+    await welcomeScreenFirstUser.goToFriends()
+    await friendsScreenFirstUser.validateFriendsScreenIsShown();
+  })
+}
