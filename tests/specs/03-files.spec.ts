b9884343b1a1eb0fab236e2ea065216977cedb4f
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2024-01-17 21:15:47 +0000

    Update tests/specs/03-files.spec.ts

--- tests/specs/03-files.spec.ts
+++ tests/specs/03-files.spec.ts
@@ -169,7 +169,7 @@ export default async function filesTests() {
     await expect(filesInfoMaxSizeLabel).toHaveTextContaining("Max Size:");
     await expect(filesInfoMaxSizeValue).toHaveTextContaining("GB");
     await expect(filesInfoCurrentSizeLabel).toHaveTextContaining(
-      "Space Used:",
+      "Used Space:",
     );
     await expect(filesInfoCurrentSizeValue).toHaveTextContaining("0 bytes");
   });
