83f6d8b0f496bf5edcd0c06c4b40f3ff5ce6dade
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-01-26 16:43:44 -0600

    chore(appium): update scripts for makefile

--- scripts/replace_node.ps1
+++ scripts/replace_node.ps1
@@ -3,8 +3,8 @@
 # Read the values from ./warp/peerID
 $localPeerId = Select-String -Path .\warp\peerID.txt -Pattern 'Local PeerID: ([^\s]*)' | ForEach-Object { $_.Matches.Groups[1].Value }
 
-# Update the values in ./common/src/warp_runner/mod.rs and store in a temporary file
-(Get-Content .\common\src\warp_runner\mod.rs) -replace '12D3KooWJSes8386p2T1sMeZ2DzsNJThKkZWbj4US6uPMpEgBTHu', $localPeerId `
-                                                      -replace '/ip4/104.236.194.35/tcp/34053', '/ip4/127.0.0.1/tcp/4444' |
-    Set-Content -Path .\common\src\warp_runner\mod.rs
+# Update the values in ./Makefile and store in a temporary file
+(Get-Content .\Makefile) -replace 'cargo build --release -F', 'SHUTTLE_ADDR_POINT=/ip4/127.0.0.1/tcp/4444/p2p/ cargo build --release -F' `
+                                                      -replace 'p2p/ ', 'p2p/ ' + $localPeerId ` |
+    Set-Content -Path .\Makefile
 
