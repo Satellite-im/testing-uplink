f41880fb0ed8d3bc35b0d1d5ea015c4d075ca31d
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-02-26 10:12:57 -0600

    chore(appium): fix duplicated code on script

--- scripts/replace_node.ps1
+++ scripts/replace_node.ps1
@@ -4,16 +4,6 @@
 $localPeerId = Select-String -Path .\warp\peerID.txt -Pattern 'Local PeerID: ([^\s]*)' | ForEach-Object { $_.Matches.Groups[1].Value }
 
 # Update the values in ./common/src/warp_runner/mod.rs and store in a temporary file
-(Get-Content .\common\src\warp_runner\mod.rs) -replace '/ip4/104.236.194.35/tcp/34053/p2p/12D3KooWJSes8386p2T1sMeZ2DzsNJThKkZWbj4US6uPMpEgBTHu', '/ip4/127.0.0.1/tcp/4444/p2p/' + $localPeerId |
-    Set-Content -Path .\common\src\warp_runner\mod.rs
-
-
-# Contents of replace_node.ps1
-
-# Read the values from ./warp/peerID
-$localPeerId = Select-String -Path .\warp\peerID.txt -Pattern 'Local PeerID: ([^\s]*)' | ForEach-Object { $_.Matches.Groups[1].Value }
-
-# Update the values in ./common/src/warp_runner/mod.rs and store in a temporary file
 (Get-Content .\common\src\warp_runner\mod.rs) -replace '/ip4/104.236.194.35/tcp/34053/', '/ip4/127.0.0.1/tcp/4444/' `
                                                       -replace 'p2p/12D3KooWJSes8386p2T1sMeZ2DzsNJThKkZWbj4US6uPMpEgBTHu', 'p2p/' + $localPeerId ` |
     Set-Content -Path .\common\src\warp_runner\mod.rs
