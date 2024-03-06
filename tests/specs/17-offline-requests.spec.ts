5ecda83c7983fca9431fbfe95856684754a88437
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-03-06 09:22:45 -0600

    test(update): fix wrong steps on offline messaging tests

--- tests/specs/17-offline-requests.spec.ts
+++ tests/specs/17-offline-requests.spec.ts
@@ -118,14 +118,10 @@ export default async function offlineRequestsTests() {
     await welcomeScreen.goToFriends();
     await friendsScreen.waitForIsShown(true);
 
-    // With UserB - Go to pending requests list, wait for receiving the friend request and accept it
-    await friendsScreen.hoverOnPendingListButton();
-    await friendsScreen.goToPendingFriendsList();
-    await friendsScreen.waitUntilUserAcceptedFriendRequest();
-
     // Validate friend is now on all friends list
     await friendsScreen.goToAllFriendsList();
     await friendsScreen.validateAllFriendsListIsShown();
     await friendsScreen.validateAllFriendsListIsNotEmpty();
+    await friendsScreen.waitUntilUserAcceptedFriendRequest();
   });
 }
