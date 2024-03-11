71709d257ea38fe68c64891c8669ee79f2453526
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-03-11 17:19:21 -0600

    chore(appium): rebase branch

--- config/wdio.shared.conf.ts
+++ config/wdio.shared.conf.ts
@@ -92,7 +92,7 @@ export const config: WebdriverIO.Config = {
   framework: "mocha",
   //
   // The number of times to retry the entire specfile when it fails as a whole
-  specFileRetries: 2,
+  specFileRetries: 0,
   //
   // Delay in seconds between the spec file retry attempts
   specFileRetriesDelay: 30,
