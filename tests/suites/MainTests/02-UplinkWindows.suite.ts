72d133a6768ee99bbbbd55e79b7c5558bc0e3e7b
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2024-03-24 23:46:55 -0600

    test(update): skip tests that need rework

--- tests/suites/MainTests/02-UplinkWindows.suite.ts
+++ tests/suites/MainTests/02-UplinkWindows.suite.ts
@@ -14,7 +14,6 @@ import settingsAboutTests from "@specs/11-settings-about.spec";
 import settingsLicensesTests from "@specs/12-settings-licenses.spec";
 import settingsDeveloperTests from "@specs/13-settings-developer.spec";
 import importAccountTests from "@specs/16-import-account.spec";
-import offlineRequestsTests from "@specs/17-offline-requests.spec";
 
 describe("MacOS Tests", function () {
   describe("Create Pin and Account Tests", createAccountTests.bind(this));
@@ -40,5 +39,5 @@ describe("MacOS Tests", function () {
   describe("Settings About Tests", settingsAboutTests.bind(this));
   describe("Settings Licenses Tests", settingsLicensesTests.bind(this));
   describe("Settings Developer Tests", settingsDeveloperTests.bind(this));
-  describe("Import Account Tests", importAccountTests.bind(this));
+  xdescribe("Import Account Tests", importAccountTests.bind(this));
 });
